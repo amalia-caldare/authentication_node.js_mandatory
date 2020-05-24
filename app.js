d16c4d5d7d4460f23b00cc4a94d65f3722543ab8
Gheorghita <amaliacaldare20@gmail.com>
2020-05-24 16:19:43 +0200

    Added credentials

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-const fs = require('fs');
-
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
--- config/mysqlCredentials.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    database: 'node_a_users',
-    user: 'root',
-    password: '1234'
-};
